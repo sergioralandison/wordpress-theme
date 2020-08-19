42b6a864249a1b148cc67031f60cd17f2d7e2ff3
Sergio <sergioralandison@outlook.com>
2020-08-19 16:57:06 +0200

    :triangular_flag_on_post: introduce new features assets

--- functions.php
+++ functions.php
@@ -3,4 +3,16 @@
     add_theme_support('title-tag');
   }
 
+  function sg_register_assets(){
+    wp_register_style('bootstrap', 'https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css');
+    wp_register_script('bootstrap', 'https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js', ['popper', 'jquery'], false, true);
+    wp_deregister_script('jquery');
+    wp_register_script('popper', 'https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js', [], false, true);
+    wp_register_script('jquery', 'https://code.jquery.com/jquery-3.5.1.slim.min.js', [], false, true);
+    wp_enqueue_style('bootstrap');
+    wp_enqueue_script('bootstrap');
+  }
+
+  add_action('after_setup_theme', 'sg_theme_supports');
+  add_action('wp_enqueue_scripts', 'sg_register_assets');
 ?>
