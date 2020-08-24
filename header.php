a824d86bf11cf4c68f5bee0031d8101418abedf7
Sergio <sergioralandison@outlook.com>
2020-08-24 16:02:30 +0200

    code header.php modified

--- header.php
+++ header.php
@@ -1,29 +1,38 @@
 <!DOCTYPE html>
-<html lang="en">
-
+<html <?php language_attributes();?>>
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <?php wp_head() ?>
+  <meta charset="<?php bloginfo('charset'); ?>">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no"/>
+  <link rel="stylesheet" href="style.css">
+  <?php wp_head() ?>
 </head>
 
-<body>
-  <nav class="container-fluid navbar navbar-expand-lg navbar-light bg-light">
-    <div class="container">
+<body <?php body_class(); ?>>
 
-    </div>
-    <a class="navbar-brand" href="#">Logo</a>
-    <!-- <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-    <span class="navbar-toggler-icon"></span>
-    </button> -->
-    <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
-      <div class="navbar-nav">
-        <a class="nav-link active" href="#">Home <span class="sr-only">(current)</span></a>
-        <a class="nav-link" href="#">Articles</a>
-        <a class="nav-link" href="#">Contact</a>
+  <?php wp_body_open(); ?>
+  <header>
+    <nav class="container-fluid navbar navbar-expand-lg">
+      <div class="container">
+        <!-- <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+        </button> -->
+        <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+          <a class="navbar-brand" href="#">Logo</a>
+          <div class="navbar-nav">
+            <a class="nav-link active" href="#">Home <span class="sr-only">(current)</span></a>
+            <a class="nav-link" href="#">Articles</a>
+            <a class="nav-link" href="#">Contact</a>
+          </div>
+        </div>
       </div>
+    </nav>
+
+    <div class="container">
+      <img src="<?php echo get_template_directory_uri(); ?>/images/road.jpg" class="img-fluid" alt="road">
     </div>
-  </nav>
+
+  </header>
+
 
 
 
