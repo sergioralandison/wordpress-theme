61b7e306bd00b7a89a937a2997321c947cf96a38
Sergio <sergioralandison@outlook.com>
2020-08-25 10:07:31 +0200

    :triangular_flag_on_post: introduce footer feature method

--- footer.php
+++ footer.php
@@ -1,4 +1,13 @@
     </div>
+    <footer>
+      <?php
+        wp_nav_menu([
+          'theme_location' => 'footer',
+          'container' => false,
+          'menu_class' => 'navba-nav mr-auto'
+        ])
+      ?>
+    </footer>
     <?php wp_footer() ?>
   </body>
 </html>
