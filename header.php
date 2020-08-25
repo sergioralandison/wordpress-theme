fceef58e8e9255069fc9cab4ee030f1a3cd5931f
Sergio <sergioralandison@outlook.com>
2020-08-25 10:24:08 +0200

    link to css file deleted

--- header.php
+++ header.php
@@ -3,7 +3,6 @@
 <head>
   <meta charset="<?php bloginfo('charset'); ?>">
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no"/>
-  <link rel="stylesheet" href="style.css">
   <?php wp_head() ?>
 </head>
 
