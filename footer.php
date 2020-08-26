b024721dd206fc7a3bd99b55cec1a8f6848bc519
Sergio <sergioralandison@outlook.com>
2020-08-26 10:54:05 +0200

    :fire: removed code

--- footer.php
+++ footer.php
@@ -1,13 +1,3 @@
-    </div>
-    <footer>
-      <?php
-        wp_nav_menu([
-          'theme_location' => 'footer',
-          'container' => false,
-          'menu_class' => 'navba-nav mr-auto'
-        ])
-      ?>
-    </footer>
     <?php wp_footer() ?>
   </body>
 </html>
